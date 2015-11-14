88fe35d2d675de566ae933717bb4ce577c26f0a9
Rad Cirskis <nad2000@gmail.com>
2015-11-14 14:55:39 +1300

    data dumplint into CSV for NLP

---
+++ dump_csv.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+for t in {keywords,metrics,product,searchkey,shop,shopurl} ; do mysql -u root bukalapak <<<"SELECT * INTO OUTFILE '/tmp/$t.csv' FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' FROM $t" ; done
