275ca98b1a99654e258d8e2b252789c23f2229d9
Rad Cirskis <nad200@gmail.com>
2015-11-14 23:28:29 +1300

    added Indonesian stemmer from https://github.com/bogdan-ivanov/IndonesianStemmer

--- dump_csv.sh
+++ dump_csv.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
+### Dump CSV from MySQL:
 for t in {keywords,metrics,product,searchkey,shop,shopurl} ; do mysql -u root bukalapak <<<"SELECT * INTO OUTFILE '/tmp/$t.csv' FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '\"' LINES TERMINATED BY '\n' FROM $t" ; done
